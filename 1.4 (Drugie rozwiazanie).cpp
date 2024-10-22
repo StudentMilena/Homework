22d5826d6df23bfd5eb8952ee7071ce44aa18776
Milena <milena.na.pjatk@gmail.com>
2024-10-22 14:07:54 +0200

    Add files via upload
    Zadanie 1. 4 - Drugie rozwiązanie (std::max)

---
+++ 1.4 (Drugie rozwiazanie).cpp
@@ -0,0 +1,29 @@
+﻿// 1.4 (Drugie rozwiazanie).cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <algorithm> 
+int main()
+{
+    std::cout << "Hello. Enter a number and check which one is the biggest";
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    std::cin >> a >> b >> c;
+    int find_the_number = std::max({ a, b, c });
+
+    std::cout << find_the_number << " " <<"it's the biggest number";
+
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
