25cd7a4940b35d5b5eef0095934828b519c4300d
Milena <milena.na.pjatk@gmail.com>
2024-10-22 14:01:09 +0200

    Update 1.3.cpp

--- 1.3.cpp
+++ 1.3.cpp
@@ -4,22 +4,22 @@
 
 int main()
 {   
-    std::cout << "Hello, user. Enter a number and check, if is even or odd.";
-    int is_even_or_odd;
-    std::cin >> is_even_or_odd;
+std::cout << "Hello, user. Enter a number and check, if is even or odd.";
+int is_even_or_odd;
+std::cin >> is_even_or_odd;
 
-    if (is_even_or_odd == 0)
-    {
-        std::cout << "The number is zero";
-    }
-    else if (is_even_or_odd % 2 == 0)
-    {
-        std::cout << "The number is positive";
-    }
-    else
-    {
-        std::cout << "The number is odd";
-    }
+if (is_even_or_odd == 0)
+{
+    std::cout << "The number is zero";
+}
+else if (is_even_or_odd % 2 == 0)
+{
+    std::cout << "The number is positive";
+}
+else
+{
+    std::cout << "The number is odd";
+}
 
 }
 
