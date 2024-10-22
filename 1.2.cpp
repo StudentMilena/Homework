7b111ba8c6f3bac91a0b5bab5c1aa48f7c378d55
Milena <milena.na.pjatk@gmail.com>
2024-10-22 13:11:55 +0200

    Add files via upload
    Napisz program, który sprawdzi, czy liczba całkowita jest dodatnia, ujemna czy może jest 0.

---
+++ 1.2.cpp
@@ -0,0 +1,36 @@
+﻿// 1.2.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello, user. Please, enter a number";
+    int number;
+    std::cin >> number;
+
+    if (number > 0)
+    {
+        std::cout << "The number is positive";
+    }
+    else if (number < 0)
+    {
+        std::cout << "The number is negative";
+    }
+    else
+    {
+        std::cout << "The number is zero";
+    }
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
