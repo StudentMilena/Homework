e0cce6b87c99b0a630ed4ab821c78b94cfe1d7aa
Milena <milena.na.pjatk@gmail.com>
2024-10-22 14:14:35 +0200

    Update 1.4 (Trzecie Rozwiazanie).cpp

--- 1.4 (Trzecie Rozwiazanie).cpp
+++ 1.4 (Trzecie Rozwiazanie).cpp
@@ -5,26 +5,12 @@
 
 int main()
 {
-    std::cout << "Hello. Enter three numbers and check which one is the biggest.";
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    std::cin >> a >> b >> c;
+std::cout << "Hello. Enter three numbers and check which one is the biggest.";
+int a = 0;
+int b = 0;
+int c = 0;
+std::cin >> a >> b >> c;
 
-    int compare = (a >= b) ? ((a >= c) ? a : c) : ((b >= c) ? b : c);
-    std::cout << "The biggest number is " << compare;
+int compare = (a >= b) ? ((a >= c) ? a : c) : ((b >= c) ? b : c);
+std::cout << "The biggest number is " << compare;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
-
-
-
