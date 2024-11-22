a84f5b9574e82105ea11394ad1c6e2776a4d2176
Milena <milena.na.pjatk@gmail.com>
2024-11-22 17:17:45 +0100

    LAB 3 - dwa miejsca po przecinku.cpp

--- LAB 3 - dwa miejsca po przecinku.cpp
+++ LAB 3 - dwa miejsca po przecinku.cpp
@@ -1,6 +1,3 @@
-﻿// dwa miejsca po przecinku.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
 #include <iostream>
 #include <math.h>
 #include <iomanip>
@@ -10,9 +7,8 @@ int main()
     std::cout << "Wprowadz liczby typu float";
     float x = 0;
     float y = 0;
-    std::cin >> x >> y;
-
-    
+    std::cin >> x >> y;    
+   
     std::cout << std::fixed << std::setprecision(2) << x + y << " " << "Dodawanie" << '\n';
     std::cout << std::fixed << std::setprecision(2) << x * y << " " << "Mnozenie" << '\n';
     std::cout << std::fixed << std::setprecision(2) << x - y << " " << "Odejmowanie" << '\n';
@@ -21,6 +17,4 @@ int main()
     {
         std::cout << std::fixed << std::setprecision(2) << (x / y) << " " << "Dzielenie" << '\n';
     }
-
-
 }
