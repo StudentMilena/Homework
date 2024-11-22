e00acf980c523a4f0ca61e117770902836514f04
Milena <milena.na.pjatk@gmail.com>
2024-11-22 18:32:52 +0100

    Update and rename trojkat pasacala.cpp to Zadanie 3.4 - LAB 3 - trojkat pascala(niedokonczone).cpp

---
+++ Zadanie 3.4 - LAB 3 - trojkat pascala(niedokonczone).cpp
@@ -0,0 +1,22 @@
+﻿// trojkat pasacala.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+#include<iostream>
+#include <vector>
+int main()
+{
+	int liczba_wierszy = 0;
+	std::cin >> liczba_wierszy;
+
+	for (int i = 1; i < liczba_wierszy; i++)
+	{
+
+		for (int j = 0; j < i; ++j)
+		{
+			std::cout << 1 << " ";
+			
+		}
+		std::cout << '\n';
+
+	}
+}
+
